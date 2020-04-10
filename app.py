a16633d1f8909ac10ee61f9a880be567559bdb85
YansenChristian <lin.yansen@gmail.com>
2020-04-10 18:41:08 +0700

    add dummy route hello world

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def respond():
     name = request.args.get("name", None)
     name = name if name is not None else "Guest"
     return name
 
+
+@app.route('/hello', methods=['GET'])
+def another_respond():
+    return "Hello World!"
+
+
 if __name__ == '__main__':
-    app.run(threaded=True, port=8000)
+    app.run(debug=True, threaded=True, port=8000)
