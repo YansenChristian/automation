61340618c743153a86637fd5fee411005679d8a3
YansenChristian <lin.yansen@gmail.com>
2020-04-11 09:05:48 +0700

    refactor code

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+# ================= DEFINE ERROR HANLDERS ================== #
+@app.errorhandler(404)
+def invalidRoute(e):
+    return jsonify({'status': 404, 'message': 'Route not found'})
 
 
-@app.route('/', methods=['GET'])
-def respond():
-    name = request.args.get("name", None)
-    name = name if name is not None else "Guest"
-    return name
+@app.errorhandler(500)
+def invalidRoute(e):
+    return jsonify({'status': 500, 'message': 'Internal server error'})
+# ================= ====================== ================= #
 
 
-@app.route('/hello', methods=['GET'])
-def another_respond():
-    return "Hello World!"
+# =============== LOAD ENVIRONMENT VARIABLES =============== #
+from dotenv import load_dotenv
+load_dotenv()
+# =============== ========================== =============== #
+
+
+# ================== REGISTER API ROUTES =================== #
+from routes.health_check_blueprint import HealthCheckBlueprint
+app.register_blueprint(HealthCheckBlueprint)
+# ================== =================== =================== #
 
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True, port=8000)
+    app.run(threaded=True, port=8000)
