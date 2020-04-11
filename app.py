c004aad23f0b13b187fb746a8414f6302c2efd6b
YansenChristian <lin.yansen@gmail.com>
2020-04-11 15:35:34 +0700

    refactor api

--- app.py
+++ app.py
@@ -4,28 +4,35 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 # ================= DEFINE ERROR HANLDERS ================== #
+
 @app.errorhandler(404)
 def invalidRoute(e):
     return jsonify({'status': 404, 'message': 'Route not found'})
 
-
 @app.errorhandler(500)
 def invalidRoute(e):
     return jsonify({'status': 500, 'message': 'Internal server error'})
-# ================= ====================== ================= #
+
 
 
 # =============== LOAD ENVIRONMENT VARIABLES =============== #
+
 from dotenv import load_dotenv
 load_dotenv()
-# =============== ========================== =============== #
+
 
 
 # ================== REGISTER API ROUTES =================== #
-from routes.health_check_blueprint import HealthCheckBlueprint
-app.register_blueprint(HealthCheckBlueprint)
-# ================== =================== =================== #
 
+from routes.health_check_blueprint import HealthCheckBlueprint
+from routes.weekdone_blueprint import WeekdoneBlueprint
+
+app.register_blueprint(HealthCheckBlueprint)
+app.register_blueprint(WeekdoneBlueprint)
+
+
+
+# ======================= RUN SERVER ======================= #
 
 if __name__ == '__main__':
     app.run(threaded=True, port=8000)
