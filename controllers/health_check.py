fc93f2a6f0ea6627ed15704f30b86577bbc0d0d0
YansenChristian <lin.yansen@gmail.com>
2020-04-14 20:54:30 +0700

    refactor code

--- controllers/health_check.py
+++ controllers/health_check.py
@@ -1,7 +1,8 @@
 import utilities.http_response as response
+from utilities.zapier_storage_client import getZapierStorageClient
 from flask_restful import Resource
 
 
 class Check(Resource):
     def get(self):
-        return response.returnMessage(200, "OK"), 200
+        return response.returnMessage(200, "OK")
