66cabb5556904cf52d71874c720b6abedc349568
Yansen Christian <lin.yansen@gmail.com>
2020-05-18 22:11:55 +0700

    add function to datetime: validate string format

--- utilities/datetime.py
+++ utilities/datetime.py
@@ -1,7 +1,14 @@
 import datetime
 
 
-def getTodayDateInFormat(format):
+def getTodayDateInFormat(dateTimeFormat):
 	nowDateTime = datetime.datetime.now()
 	nowDateTime += datetime.timedelta(hours=7)
-	return nowDateTime.strftime(format)
+	return nowDateTime.strftime(dateTimeFormat)
+
+
+def validateStringFormat(dateTimeText, dateTimeFormat):
+	try:
+		datetime.datetime.strptime(dateTimeText, dateTimeFormat)
+	except ValueError:
+		raise ValueError("Incorrect data format, should be " + dateTimeFormat)
